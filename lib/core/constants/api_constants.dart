9244979f20137126c4d389e990da967a9a2e4ba5
noyon-360 <nazibullahnoyon19.20@gmail.com>
2026-05-04 08:29:50 +0600

    switch base domain to production API endpoint

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,9 +1,9 @@
 class ApiConstants {
   /// [Base Configuration]
-  static const String baseDomain = 'http://10.10.5.48:5001'; // Noyon Office
+  // static const String baseDomain = 'http://10.10.5.48:5001'; // Noyon Office
   // static const String baseDomain = 'http://192.168.0.218:5000'; // Noyon Home
 
-  // static const String baseDomain = 'https://api.labbytv.com'; // PRODUCTION
+  static const String baseDomain = 'https://api.labbytv.com'; // PRODUCTION
 
   static const String baseUrl = '$baseDomain/api/v1';
 
